f8fa029bc88a97fac1f1962f97650f3f9f3d3626
LordCreepity <johnstratton679@yahoo.com>
2015-12-08 21:51:49 -0500

    Added the capability to output an object file to the assembler.

--- asm.c
+++ asm.c
@@ -2,17 +2,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int main(int argc, char **argv){
     FILE *asmsource;
     char *line = NULL;
     size_t len = 0;
+    int outputImageFD;
 
     asmsource = fopen(argv[1], "r");
     if (stream == NULL){
         fprintf(stderr, "%s: %s: No such file or directory\n", argv[0],
             argv[1]);
         exit(1);
+    }
+    outputImageFD = open("image.sdm", O_WRONLY, O_CREAT);
+    if (outputImageFD == -1){
+        perror("cannot create output file");
+        exit(2);
     }
     while (getline(&line, &len, asmsource) != -1) {
         
