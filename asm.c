dd3acffc309a5bba588c649bf25967b6624d6456
LordCreepity <johnstratton679@yahoo.com>
2015-12-09 13:05:09 -0500

    The asm parser for the assembler is a little under halfway complete now.

--- asm.c
+++ asm.c
@@ -7,16 +7,21 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <stdint.h>
+#include <unistd.h>
 
 int main(int argc, char **argv){
     FILE *asmsource;
     char *line = NULL;
     size_t len = 0;
     int outputImageFD;
+    char *mnemonic;
+    int16_t instruction;
+    char *addressString = "a";
 
     asmsource = fopen(argv[1], "r");
     if (stream == NULL){
-        fprintf(stderr, "%s: %s: No such file or directory\n", argv[0],
+        fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], 
             argv[1]);
         exit(1);
     }
@@ -26,6 +31,28 @@ int main(int argc, char **argv){
         exit(2);
     }
     while (getline(&line, &len, asmsource) != -1) {
-        
+        mnemonic = strtok(line, " ");
+        if (!strcmp(mnemonic, "nop")){
+            instruction = 0;
+        } else if (!strcmp(mnemonic, "adda")){
+            addressString = strtok(NULL, " ");
+            instruction = 0x1000 + (int16_t)strtol(addressString, NULL, 0);
+        } else if (!strcmp(mnemonic, "addm")){
+            addressString = strtok(NULL, " ");
+            instruction = 0x2000 + (int16_t)strtol(addressString, NULL, 0);
+        } else if (!strcmp(mnemonic, "suba")){
+            addressString = strtok(NULL, " ");
+            instruction = 0x3000 + (int16_t)strtol(addressString, NULL, 0);
+        } else if (!strcmp(mnemonic, "subm")){
+            addressString = strtok(NULL, " ");
+            instruction = 0x4000 + (int16_t)strtol(addressString, NULL, 0);
+        } else if (!strcmp(mnemonic, "cla")){
+            instruction = 0x5000;
+        } else if (!strcmp(mnemonic, "clm")){
+            addressString = strtok(NULL, " ");
+            instruction = 0x6000 + (int16_t)strtol(addressString, NULL, 0);
+        } else if (!strcmp(mnemonic, "jmp")){
+            
+        }
     }
 }
